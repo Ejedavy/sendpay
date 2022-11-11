86a7509f06c28849fde468fdade16c6a5481528a
David Eje <davideje@Davids-MacBook-Pro.local>
2022-11-11 19:34:41 +0300

    Fixing the core

--- internal/core/interfaces.go
+++ internal/core/interfaces.go
@@ -1,14 +1,12 @@
 package core
 
-type TransferInterfae interface {
-	MakeTransfer(Account, Account) (Account, Transaction, error)
-	MakeRefund(Transaction) (Transaction, error)
-}
-
 type UserInterface interface {
 	CreateUser(string) (User, error)
 }
 
 type AccountInterface interface {
 	CreateAccount(string) (Account, error)
+	TopUp(Account, float32) (Account, error)
+	MakeTransfer(Account, Account) (Account, Transaction, error)
+	MakeRefund(Transaction) (Transaction, error)
 }
